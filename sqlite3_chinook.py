545aaaee9fe73ce63a0ded1f8f6745915104e54b
Bikramjit Bhullar <32780436+bsb00004@users.noreply.github.com>
2019-12-14 18:56:46 +0000

    added comments
    to understand the code better later in future , comments has been added

--- sqlite3_chinook.py
+++ sqlite3_chinook.py
@@ -1,29 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ### Project 10-Answering Business Questions using SQL
-# 
-# In this project, we're going to answer business questions using our SQL skills like: SQL joining, using subqueries, multiple joins, set operations, aggregate functions and more. We are using diffrent tasks in every steps to go through all of the SQL skills:
-# 
+### Project 10-Answering Business Questions using SQL
+
+# In this project, we're going to answer business questions using our SQL skills like:  
+# We are using diffrent tasks in every steps to go through all of the SQL skills:
+
 # - Write a SQL query to extract the relevant data
 # - Create plots where necessary to visualize the data
 # - Write a short paragraph, drawing conclusions and explaining the data and/or visualizations.
-# 
-# We are using the Chinook database, which is provided as a SQLite database file called __chinook.db__. A copy of the database schema is below - we'll need to come back to this step often to consult the schema as we write your queries.
-# <img src="https://s3.amazonaws.com/dq-content/191/chinook-schema.svg" />
-# 
-# It's worth remembering that our database retains 'state', so if we run a query with a CREATE or DROP twice, the query will fail.  But if you have trouble, or if you manage to lock your database, we have provided a __chinook-unmodified.db__ file that you can copy over the __chinook.db__ to restore it back to its initial state (see [this blog post on how to run shell commands within a Jupyter notebook](https://www.dataquest.io/blog/jupyter-notebook-tips-tricks-shortcuts/#17executingshellcommands)).
-# 
-# Here are a few tips to keep in mind while working on these queries:
-# 
-# - Write your query in stages, and run it as you go to make sure at each stage it's producing the output you expect.
-# - If something isn't behaving as you expect, break parts of the query out into their own, separate queries to make sure there's not an inner logic error.
-# - Don't be afraid to write separate queries to check the underlying data, for instance you might write a query that you can use to manually check a calculation and give yourself confidence that the output you're seeing is correct.
-# 
+
 # ### Creating Helper Functions
-
-# In[1]:
-
 
 # Importing the SQLite, pandas and matplotlib modules
 import sqlite3
@@ -31,11 +18,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
+
 # magic command %matplotlib inline 
 # to make sure any plots render in the notebook
 get_ipython().magic('matplotlib inline')
 
+# assgning databse to variable
 db = 'chinook.db'
+
 # Creating a run_query() function, that takes 
 # a SQL query as an argument and returns 
 # a pandas dataframe of that query
@@ -73,9 +63,6 @@ show_tables()
 # - in absolute numbers
 # - in percentages.
 
-# In[2]:
-
-
 albums_to_purchase = '''
 WITH usa_tracks_sold AS
    (
@@ -102,11 +89,7 @@ LIMIT 10;
 run_query(albums_to_purchase)
 
 
-# ### Creating a plot to show the above data.
-
-# In[3]:
-
-
+### Creating a plot to show the above data.
 genre_sales_usa = run_query(albums_to_purchase)
 genre_sales_usa.set_index("genre", inplace=True, drop=True)
 
@@ -126,20 +109,9 @@ for i, label in enumerate(list(genre_sales_usa.index)):
 
 plt.show()
 
-
-# Based on the sales of tracks across different genres in the USA, we should purchase the new albums by the following artists:
-# 
-# - Red Tone (Punk)
-# - Slim Jim Bites (Blues)
-# - Meteor and the Girls (Pop)
-# 
-# It's worth keeping in mind that combined, these three genres only make up only 17% of total sales, so we should be on the lookout for artists and albums from the 'rock' genre, which accounts for 53% of sales.
-# 
-# ### Analyzing Employee Sales Performance
-# Writing a query that finds the total dollar amount of sales assigned to each sales support agent within the company. Add any extra attributes for that employee that you find are relevant to the analysis.
-
-# In[4]:
-
+### Analyzing Employee Sales Performance
+# Writing a query that finds the total dollar amount of sales assigned to each sales support agent within the company. 
+# Add any extra attributes for that employee that you find are relevant to the analysis.
 
 employee_sales_performance = '''
 WITH customer_support_rep_sales AS
@@ -164,11 +136,7 @@ GROUP BY 1;
 
 run_query(employee_sales_performance)
 
-
-# ### Creating a plot of the results of the above query
-
-# In[5]:
-
+### Creating a plot of the results of the above query
 
 employee_sales = run_query(employee_sales_performance)
 
@@ -182,21 +150,8 @@ employee_sales.plot.barh(
 plt.ylabel('')
 plt.show()
 
-
-# While there is a 20% difference in sales between Jane (the top employee) and Steve (the bottom employee), the difference roughly corresponds with the differences in their hiring dates.
-# 
-# ### Analyzing Sales by Country
+### Analyzing Sales by Country
 # Writing a query that collates data on purchases from different countries.
-# - Where a country has only one customer, collect them into an "Other" group.
-# - The results should be sorted by the total sales from highest to lowest, with the "Other" group at the very bottom.
-# - For each country, include:
-#     - total number of customers
-#     - total value of sales
-#     - average value of sales per customer
-#     - average order value
-
-# In[6]:
-
 
 sales_by_country = '''
 WITH country_or_other AS
@@ -244,20 +199,13 @@ FROM
 run_query(sales_by_country)
 
 
-# ### Visualizing Sales by Country
-# 
-# Now that we have our data, we've to create a series of visualizations which communicate our findings, and then make recommendations on which countries may have potential for growth, so the Chinook marketing team can create some new advertising campaigns.
-# 
-# When we're working with data that has many dimensions, it can be tempting to try and communicate every dimension in a single plot. This often results in complex and hard to read visualizations. Instead of this, we should create different plots for each dimension.
-# 
-# The best way to communicate the data - this includes not only plot types, but how you use color, spacing and layout.
-# 
+### Visualizing Sales by Country
+
+# Now that we have our data, we've to create a series of visualizations which communicate our findings, 
+# and then make recommendations on which countries may have potential for growth, 
+# so the Chinook marketing team can create some new advertising campaigns.
+
 # For each dimension, creating a visualization which demonstrates the data we collated in the previous step.
-# - We should also decide whether the "Other" group is relevant to the analysis and make decisions on where to include it (if anywhere) in the visualizations.
-
-# In[7]:
-
-
 country_metrics = run_query(sales_by_country)
 country_metrics.set_index("country", drop=True, inplace=True)
 colors = [plt.cm.Accent(i) for i in np.linspace(0, 1, country_metrics.shape[0])]
@@ -328,22 +276,17 @@ ax4.spines["right"].set_visible(False)
 plt.show()
 
 
-# Based on the data, there may be opportunity in the following countries:
-# 
+# Based on the data, there may be opportunity in the following countries: 
 # - Czech Republic
 # - United Kingdom
 # - India
-# 
-# It's worth keeping in mind that because the amount of data from each of these countries is relatively low. Because of this, we should be cautious spending too much money on new marketing campaigns, as the sample size is not large enough to give us high confidence. A better approach would be to run small campaigns in these countries, collecting and analyzing the new customers to make sure that these trends hold with new customers.
-# 
-# ### Albums vs Individual Tracks
-# 
-# Writing a query that categorizes each invoice as either an album purchase or not, and calculates the following summary statistics:
+
+### Albums vs Individual Tracks
+
+# Writing a query that categorizes each invoice as either an album purchase or not, 
+# and calculates the following summary statistics:
 # - Number of invoices
 # - Percentage of invoices
-
-# In[8]:
-
 
 albums_vs_tracks = '''
 WITH invoice_first_track AS
@@ -402,20 +345,6 @@ GROUP BY album_purchase;
 
 run_query(albums_vs_tracks)
 
-
-# Album purchases account for 18.6% of purchases. Based on this data, I would recommend against purchasing only select tracks from albums from record companies, since there is potential to lose one fifth of revenue.
-# 
-# ### Further practice
-# 
-# We can practice more, like we can look at the schema and come up with some more business questions, and then write queries to answer them. Here are a few to get you started:
-# 
-# - Which artist is used in the most playlists?
-# - How many tracks have been purchased vs not purchased?
-# - Is the range of tracks in the store reflective of their sales popularity?
-# - Do protected vs non-protected media types have an effect on popularity?
-
-# In[ ]:
-
-
-
-
+# Album purchases account for 18.6% of purchases. Based on this data, 
+# I would recommend against purchasing only select tracks from albums from record companies, 
+# since there is potential to lose one fifth of revenue.
